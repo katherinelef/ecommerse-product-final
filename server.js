b5396fc9a0214903d2ad30dafa08bc86ebec6c20
betsy18 <betsy_dessire@hotmail.com>
2018-02-24 13:41:58 -0500

    Agregando readme y gitignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();//guardar expres en una var
-const server = app.listen(3000, encender);//función de callback
-function encender () {
-    console.log('servidor encendido');
+const app = express();// guardar expres en una var
+const server = app.listen(3000, encender);// función de callback
+function encender() {
+  console.log('servidor encendido');
 }
-app.use(express.static('public'));//utilizar .use express.static
+app.use(express.static('public'));// utilizar .use express.static
