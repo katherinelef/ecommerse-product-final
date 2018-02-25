abef2ce946ff3db6cfadc240ee4fa71b84dcc310
katherinelef <katherine.lef@hotmail.com>
2018-02-24 22:58:15 -0500

    añadiendo imagenes productos

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,13 @@ function getData() {
       var result = response.results;
 
       $.each(result, function(index, obj) {
-        $('.content').append(`<div><p>Title: ${result[index].title}</p></div>`)
+        $('.content').append(`<div class="card col-4">
+        <img class="card-img-top" src="${result[index].thumbnail}" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">${result[index].title}</p>
+          <p class="card-text">S/.${result[index].price}</p>
+        </div>
+      </div>`)
         console.log(result[index].title);
       });
 
@@ -36,5 +42,32 @@ function getData() {
   });
   
 }
+
+$('#categorias').click(function () {
+ 
+  getCategories();
+});
+
+function  getCategories() {
+
+  $.ajax({
+    url: `https://api.mercadolibre.com/sites/MLA/search?category=MLA1273`,
+    contentType: 'application/json',
+    method: 'GET',
+    success: function(response) {
+      console.log(response);
+      
+
+    
+    },
+    fail: function(request) {
+      if (request) {
+        alert(request.message);
+      }
+    }
+  });
+  
+}
+
 
 
