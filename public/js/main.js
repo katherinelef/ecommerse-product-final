3e8b0c02d25bdf3c86a9fbd6986203e7bc63dac5
betsy18 <betsy_dessire@hotmail.com>
2018-02-26 06:22:17 -0500

    Mejorando la vista e implementando minicart.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 
 var form = $('#search-form');
-  
+
 var search = $('#searchProducts');
 let searchedForText;
 
@@ -20,14 +20,22 @@ function getData() {
       var result = response.results;
 
       $.each(result, function(index, obj) {
-        $('.content').append(`<div class="card col-4">
-        <img class="card-img-top img-responsive" src="${result[index].thumbnail}" alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">${result[index].title}</p>
-          <p class="card-text">S/.${result[index].price}</p>
+        $('.content').append(`
+        <div class="card-deck">
+          <div class="card col-xs-12 col-sm-4 col-md-4">
+          <img class="card-img-top tam" src=${result[index].thumbnail}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${result[index].title}</h5>
+            <p class="card-text">S/.${result[index].price}</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"><i class="fa  fa-2x fa-cart-plus" aria-hidden="true"></i></small>
+            <a href="#" class="btn btn-default producto" precio="${result[index].price}" titulo="${result[index].title}"role="button">Comprar</a>
+          </div>
         </div>
-      </div>`);
+        </div>`);
         console.log(result[index].title);
+        // $('img.tam').attr('src', '');
       });
     },
     fail: function(request) {
@@ -57,5 +65,26 @@ function getCategories() {
     }
   });
 }
+
+//  carro de compras
+
+paypal.minicart.render({
+  strings: {
+    button: 'Pagar'
+    , buttonAlt: 'Total'
+    , subtotal: 'Total:'
+    , empty: 'No hay productos en el carrito'
+  }
+});
+
+$('.producto').click(function(e) {
+  e.stopPropagation();
+  paypal.minicart.cart.add({
+    // business: 'uhperezoscar@gmail.com', // Cuenta paypal para recibir el dinero
+    itemName: $(this).attr('titulo'),
+    amount: $(this).attr('precio'),
+    currencyCode: 'USD',
+  });
+});
 
 
