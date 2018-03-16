5599d87f719ae3de781413f1877a6b25748d3140
Betsy Lara <betsy_dessire@hotmail.com>
2018-03-16 04:49:26 -0500

    modificando css

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,8 @@ var form = $('#search-form');
 var search = $('#searchProducts');
 let searchedForText;
 
-form.submit(function(e) {
-  e.preventDefault();
+form.submit(function(ev) {
+  ev.preventDefault();
   searchedForText = search.val();
   getData();
 });
@@ -19,23 +19,21 @@ function getData() {
       console.log(response.results);
       var result = response.results;
 
-      $.each(result, function(index, obj) {
+      $.each(result, function(index) {
         $('.content').append(`
-        <div class="card-deck">
-          <div class="card col-xs-12 col-sm-4 col-md-4">
+        <div class="card col-xs-12 col-ms-4 col-md-4">
           <img class="card-img-top tam" src=${result[index].thumbnail}" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">${result[index].title}</h5>
             <p class="card-text">S/.${result[index].price}</p>
+            <p class="card-text">Cantidad: ${result[index].available_quantity}</p>            
           </div>
           <div class="card-footer">
             <small class="text-muted"><i class="fa  fa-2x fa-cart-plus" aria-hidden="true"></i></small>
-            <input class="btn btn-outline-info producto" type="submit" titulo="${result[index].title}" precio="${result[index].price}" value="Comprar">
+            <input class="btn btn-outline-info producto" type="button" titulo="${result[index].title}" precio="${result[index].price}" value="Comprar">
           </div>
-        </div>
         </div>`);
         console.log(result[index].title);
-        // $('img.tam').attr('src', '');
         
         //  carro de compras
         paypal.minicart.render({
@@ -47,8 +45,13 @@ function getData() {
           }
         });
 
-        $('.producto').click(function(e) {
-          e.stopPropagation();
+        $('#viewcart').click(function(ev) {
+          ev.stopPropagation();
+          paypal.minicart.view.show();
+        });
+
+        $('.producto').click(function(ev) {
+          ev.stopPropagation();
           paypal.minicart.cart.add({
             business: 'test@gmail.com', 
             item_name: $(this).attr('titulo'),
